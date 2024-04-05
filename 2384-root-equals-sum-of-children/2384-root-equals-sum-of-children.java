be5526b2713bf96be1656126db59546708730849
bhavya-020 <92467823+bhavya-020@users.noreply.github.com>
2024-04-05 17:57:39 +0530

    Time: 0 ms (100%), Space: 41.2 MB (49.77%) - LeetHub

---
+++ 2384-root-equals-sum-of-children/2384-root-equals-sum-of-children.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean checkTree(TreeNode root) {
+        
+        if(root.val == root.left.val + root.right.val){
+            return true;
+        }
+        return false;
+    }
+}
