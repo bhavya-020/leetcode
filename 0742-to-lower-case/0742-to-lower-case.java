cc4a66c734e3e15943749926a012b04f796d86b3
bhavya-020 <92467823+bhavya-020@users.noreply.github.com>
2024-04-09 23:32:52 +0530

    Time: 0 ms (100%), Space: 41.4 MB (54.88%) - LeetHub

---
+++ 0742-to-lower-case/0742-to-lower-case.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String toLowerCase(String s) {
+
+
+
+        // System.out.print(s.toLowerCase());
+        return s.toLowerCase();
+
+        // return s;
+        // return -1;
+    }
+}
