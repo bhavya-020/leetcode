59f6cb0de769001727b76215718c6937738d91b1
bhavya-020 <92467823+bhavya-020@users.noreply.github.com>
2024-04-24 14:16:42 +0530

    Time: 0 ms (100%), Space: 40.4 MB (23.43%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -3,13 +3,18 @@ class Solution {
 
         if(n==0){return 0;}
         else if(n==1 || n==2){return 1;}
-        else {return (tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3));}
-        // int a = 0 , b = 1 , c = 1;
-        // int ans = 0;
-        // for(int i = 0;i<n-2;i++){
+        // else {return (tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3));}
+        int a = 0 , b = 1 , c = 1;
+        int ans = 0;
+        for(int i = 3;i<=n;i++){
         //     ans = ans+i;
+        ans = a + b+ c;
+        a = b;
+        b = c;
+         c = ans;
+        }
 
-        // return ans + a+b+c;
+        return ans ;
         
     }
 }
